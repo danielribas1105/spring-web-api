4c493b731a2e3333c8b9df4f9d605c9caec4174a
Daniel <danielribas1105@gmail.com>
2023-04-19 15:17:50 -0300

    insert welcome controller

---
+++ src/main/java/drcode/webapi/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package drcode.webapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to my Spring Boot Web API";
+    }
+}
