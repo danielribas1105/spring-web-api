f9847a746d20e2b41ce1cf9ef4fcf9ae36af897d
Daniel <danielribas1105@gmail.com>
2023-04-19 18:25:22 -0300

    update spring api

---
+++ src/main/java/drcode/webapi/controller/UsuarioController.java
@@ -0,0 +1,31 @@
+package drcode.webapi.controller;
+
+import drcode.webapi.model.Usuario;
+import drcode.webapi.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository repository;
+    @GetMapping()
+    public List<Usuario> getUsers(){
+        return repository.listAll();
+    }
+    @GetMapping("/{username}")
+    public Usuario getOne(@PathVariable("username") String username){
+        return repository.findByUserName(username);
+    }
+    @DeleteMapping("/{userid}")
+    public void deleteUser(@PathVariable("userid") Integer id){
+        repository.remove(id);
+    }
+    @PostMapping()
+    public void postUser(@RequestBody Usuario usuario){
+        repository.save(usuario);
+    }
+}
